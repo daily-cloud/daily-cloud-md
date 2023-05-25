a2275f1b9b30dda81376aaddd43ef3a0576f6206
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2023-05-25 13:50:12 +0700

    [Journal & Mood Tracker] Tampilan di Home

--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/home/HomeScreen.kt
@@ -1,8 +1,39 @@
 package com.dailycloud.dailycloud.ui.screen.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.dailycloud.dailycloud.R
+import com.dailycloud.dailycloud.ui.common.Mood
+import com.dailycloud.dailycloud.ui.components.JournalPreview
+import com.dailycloud.dailycloud.ui.components.MoodChoices
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel(),
+    toJournal: () -> Unit,
+) {
+    val isSelected by viewModel.isMoodSelected
+    val moodSelected by viewModel.selectedMood
+    val moodHistory by viewModel.moodHistory
+    val journalContent by viewModel.journalContent
+
+    Column(modifier = modifier) {
+        Text("Hello, Cloudie!", style = MaterialTheme.typography.displaySmall, modifier = Modifier.padding(16.dp))
+        MoodChoices(isSelected = isSelected, moodSelected = moodSelected, moodHistory = moodHistory, onMoodSelected = viewModel::onMoodSelected)
+        JournalPreview(journalContent = journalContent, toJournal = toJournal)
+    }
 
 }
--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/home/HomeViewModel.kt
@@ -1,10 +1,38 @@
 package com.dailycloud.dailycloud.ui.screen.home
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dailycloud.dailycloud.data.DailyCloudRepository
+import com.dailycloud.dailycloud.ui.common.Mood
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: DailyCloudRepository) : ViewModel() {
+
+    private val _isMoodSelected: MutableState<Boolean> = mutableStateOf(false)
+    val isMoodSelected: State<Boolean> = _isMoodSelected
+
+    private val _selectedMood: MutableState<Mood?> = mutableStateOf(null)
+    val selectedMood: State<Mood?> = _selectedMood
+
+    private val _moodHistory: MutableState<List<Mood>> = mutableStateOf(emptyList())
+    val moodHistory: State<List<Mood>> = _moodHistory
+
+    private val _journalContent: MutableState<String?> = mutableStateOf(null)
+    val journalContent: State<String?> = _journalContent
+
+    fun onMoodSelected(mood: Mood) {
+        _isMoodSelected.value = true
+        _selectedMood.value = mood
+        _moodHistory.value = listOf(Mood.HappyMood, Mood.NeutralMood, Mood.SadMood)
+    }
+
 }
