f0afad21261a654eb8f238fedde8a2d75260363d
KaisarV <kaisar.valentino123@gmail.com>
2023-05-30 03:35:42 +0700

    [Profile] Profile sketch

--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/profile/ProfileScreen.kt
@@ -1,11 +1,93 @@
 package com.dailycloud.dailycloud.ui.screen.profile
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 @Composable
-fun ProfileScreen(
-    modifier: Modifier = Modifier,
-) {
+fun ProfileScreen() {
+    val profileImageUrl = "https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+    val name = "John Doe"
+    val email = "johndoe@example.com"
+    val bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
 
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            AsyncImage(
+                model = profileImageUrl,
+                contentDescription = "Profile Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Name",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+
+            Text(
+                text = name,
+                style = TextStyle(fontSize = 16.sp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Email",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+
+            Text(
+                text = email,
+                style = TextStyle(fontSize = 16.sp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Bio",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            )
+
+            Text(
+                text = bio,
+                style = TextStyle(fontSize = 16.sp)
+            )
+        }
+    }
 }
+
+
