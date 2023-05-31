a6037a475911cf2b6d7d53e3b135b007f10aaafa
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2023-05-31 14:32:13 +0700

    [Content] Merapihkan Halaman Contents
    - exoplayer media3

--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/content/ContentScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/content/ContentScreen.kt
@@ -12,22 +12,34 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.dailycloud.dailycloud.ui.common.UiState
+import com.dailycloud.dailycloud.ui.theme.DailyCloudTheme
+import com.dailycloud.dailycloud.ui.theme.Primary
 import com.dailycloud.dailycloud.util.Util.dateFormat
+import com.google.firebase.Timestamp
 
 @Composable
 fun ContentScreen(
@@ -48,7 +60,8 @@ fun ContentScreen(
                     data.title,
                     data.article,
                     data.author,
-                    dateFormat(data.createdAt)
+                    dateFormat(data.createdAt),
+                    backToContents = navigateBack,
                 )
             }
 
@@ -64,55 +77,75 @@ fun DetailContent(
     article: String,
     author: String,
     releaseDate: String,
+    backToContents: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.White)
+    Column(
+        modifier = modifier.verticalScroll(rememberScrollState())
     ) {
-        Column(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .padding(16.dp)
-        ) {
+        Box() {
             AsyncImage(
                 model = photoUrl,
                 contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(500.dp)
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().clip(shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp))
             )
-
-            Text(
-                text = title,
-                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            Row{
-
-                Text(
-                    text = author,
-                    style = TextStyle(fontSize = 16.sp),
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-
-                Spacer(modifier = Modifier.width(16.dp))
-
-                Text(
-                    text = releaseDate,
-                    style = TextStyle(fontSize = 16.sp, fontStyle = FontStyle.Italic),
-                    modifier = Modifier.padding(bottom = 16.dp)
+            IconButton(onClick = backToContents) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    tint = Color.White,
                 )
             }
-
+        }
+        Text(
+            text = title,
+            style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)
+        )
+        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
 
             Text(
-                text = article,
-                style = TextStyle(fontSize = 16.sp, lineHeight = 20.sp),
-                color = Color.Black,
+                text = author,
+                style = TextStyle(fontSize = 16.sp),
                 modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = releaseDate,
+                style = TextStyle(fontSize = 16.sp, fontStyle = FontStyle.Italic),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+        Text(
+            text = article,
+            style = TextStyle(fontSize = 16.sp, lineHeight = 20.sp),
+            color = Color.Black,
+            modifier = Modifier.padding(bottom = 16.dp, start = 16.dp, end = 16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ContentPreview() {
+    DailyCloudTheme() {
+        Surface() {
+            DetailContent(
+                photoUrl = "",
+                title = "Kamu sering dengar “aduh kena mental”, lantas apa itu kesehatan mental?",
+                "Sebagian dari kita mungkin sering mendegar kata-kata “aduh mental gue kena” dan istilah sejenis. Tetapi, kamu tahu **ga** apa itu kesehatan mental? Jangan-jangan kamu sering dengar atau bahkan mengakatan istilah tersebut tanpa tahu makna dari istilah tersebut. Kalau belum, tenang saja, yuk sama-sama belajar mengenai apa itu kesehatan mental.\n" +
+                        "\n" +
+                        "Menurut organisasi kesehatan dunia, WHO, Kesehatan mental adalah keadaan kesejahteraan mental yang memungkinkan orang mengatasi tekanan hidup, menyadari kemampuan mereka, belajar dengan baik dan bekerja dengan baik, dan berkontribusi pada komunitas mereka.\n" +
+                        "\n" +
+                        "Kesehatan mental lebih dari tidak adanya gangguan mental. Itu ada dalam rangkaian kesatuan yang kompleks, yang dialami secara berbeda dari satu orang ke orang lain, dengan tingkat kesulitan dan kesusahan yang berbeda-beda dan hasil sosial dan klinis yang berpotensi sangat berbeda\n" +
+                        "\n" +
+                        "Nah, jadi itu ya mengenai apa itu kesehatan mental. Jadi, setelah membaca ini, teman-teman bisa memahami apa itu kesehatan mental",
+                "Daily Cloud",
+                "31 May 2021",
+                {}
             )
         }
     }
--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/contents/ContentsScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/contents/ContentsScreen.kt
@@ -1,44 +1,42 @@
 package com.dailycloud.dailycloud.ui.screen.contents
 
-
 import android.content.res.Resources
 import android.net.Uri
 import android.view.ViewGroup
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.dailycloud.dailycloud.R
 import com.dailycloud.dailycloud.data.local.model.Content
 import com.dailycloud.dailycloud.ui.common.UiState
 import com.dailycloud.dailycloud.ui.components.ContentListItem
+import com.dailycloud.dailycloud.ui.theme.DailyCloudTheme
 import com.dailycloud.dailycloud.util.Util
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.ui.StyledPlayerView
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
-
+import com.dailycloud.dailycloud.util.Util.dpToPx
 
 @Composable
 fun ContentsScreen(
@@ -51,6 +49,7 @@ fun ContentsScreen(
             is UiState.Loading -> {
                 viewModel.getContents()
             }
+
             is UiState.Success -> {
                 Content(
                     contentList = uiState.data,
@@ -58,94 +57,83 @@ fun ContentsScreen(
                     navigateToContentDetail = navigateToContentDetail,
                 )
             }
+
             is UiState.Error -> {}
         }
     }
 
 }
+
 @Composable
 fun Content(
     contentList: List<Content>,
     modifier: Modifier = Modifier,
     navigateToContentDetail: (String) -> Unit,
 ) {
-
-    Box(modifier = modifier) {
-        Column(
-            Modifier.padding(top = 40.dp, start = 40.dp, end = 40.dp)
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.content),
+            style = MaterialTheme.typography.displaySmall,
+        )
+        Text(
+            text = stringResource(R.string.content_quote),
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            state = rememberLazyListState(),
         ) {
-            Text(
-                text = stringResource(R.string.content),
-                color = Color.Black,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp)
-            )
-            Text(
-                text = stringResource(R.string.content_quote),
-                color = Color.Black,
-                modifier = Modifier.padding(bottom = 20.dp)
-            )
-
-//            AndroidView(factory = {
-//                val view = YouTubePlayerView(it)
-//                view.addYouTubePlayerListener(
-//                    object : AbstractYouTubePlayerListener() {
-//                        override fun onReady(youTubePlayer: YouTubePlayer) {
-//                            super.onReady(youTubePlayer)
-//                            youTubePlayer.loadVideo("hsfScVJgGOw", 0f)
-//                        }
-//                    }
-//                )
-//                view
-//            },
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(300.dp)
-//                    .padding(bottom = 20.dp))
-
-            VideoPlayer("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
-
-            LazyColumn{
-                items(contentList) { data ->
-                    ContentListItem(
-                        title = data.title,
-                        articlePreview = Util.reduceText(data.article, 100),
-                        photoUrl = data.photoPath,
-                        modifier = Modifier.clickable{ navigateToContentDetail(data.contentId)}
-                            .fillMaxWidth().padding(top=35.dp))
-                }
+            item {
+                VideoPlayer("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            items(contentList) { data ->
+                ContentListItem(
+                    title = data.title,
+                    articlePreview = Util.reduceText(data.article, 100),
+                    photoUrl = data.photoPath,
+                    onClick = { navigateToContentDetail(data.contentId) },
+                )
             }
         }
     }
 }
 
 @Composable
-fun VideoPlayer(url : String) {
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+fun VideoPlayer(url: String) {
     val context = LocalContext.current
     val exoPlayer = ExoPlayer.Builder(context).build()
     val mediaItem = MediaItem.fromUri(Uri.parse(url))
     exoPlayer.setMediaItem(mediaItem)
 
-    fun Int.dpToPx(): Int {
-        val scale = Resources.getSystem().displayMetrics.density
-        return (this * scale + 0.5f).toInt()
-    }
-
-    val playerView = StyledPlayerView(context)
+    val playerView = PlayerView(context)
     playerView.player = exoPlayer
+    playerView.setShowShuffleButton(false)
+    playerView.setShowNextButton(false)
+    playerView.setShowPreviousButton(false)
+    playerView.setShowFastForwardButton(false)
+    playerView.setShowRewindButton(false)
     playerView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300.dpToPx())
 
-    DisposableEffect(AndroidView(factory = {playerView})){
+    DisposableEffect(
+        AndroidView(
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+            factory = { playerView }
+        )
+    ) {
 
         exoPlayer.prepare()
-        exoPlayer.playWhenReady= true
+        exoPlayer.playWhenReady = false
 
         onDispose {
             exoPlayer.release()
         }
     }
-
-
 }
 
