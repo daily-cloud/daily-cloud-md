262c8b947ecb11b5a15753d1407ab8a1b7670040
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2023-06-12 17:14:29 +0700

    [History] Tampilan Detail Jurnal

--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/history/HistoryScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/history/HistoryScreen.kt
@@ -1,9 +1,12 @@
 package com.dailycloud.dailycloud.ui.screen.history
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -21,6 +24,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -31,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dailycloud.dailycloud.R
+import com.dailycloud.dailycloud.ui.common.UiState
 import com.dailycloud.dailycloud.ui.components.CustomFilledButton
 import com.dailycloud.dailycloud.ui.theme.DailyCloudTheme
 import com.dailycloud.dailycloud.ui.theme.Primary
@@ -41,9 +48,13 @@ import com.dailycloud.dailycloud.util.Util.toDate
 fun HistoryScreen(
     modifier: Modifier = Modifier,
     viewModel: HistoryViewModel = hiltViewModel(),
-    toJournal: () -> Unit,
+    toJournal: (String) -> Unit,
 ) {
-    val datePickerState = rememberDatePickerState(initialSelectedDateMillis = System.currentTimeMillis())
+    val datePickerState = viewModel.datePickerState
+    val selectedDateJournal by viewModel.selectedDateJournal
+    LaunchedEffect(key1 = datePickerState.selectedDateMillis) {
+        viewModel.onDateChanged(datePickerState.selectedDateMillis ?: System.currentTimeMillis())
+    }
     Column(
         modifier = modifier
             .padding(16.dp)
@@ -51,7 +62,10 @@ fun HistoryScreen(
             .verticalScroll(rememberScrollState())
     ) {
         Text(stringResource(R.string.history), style = MaterialTheme.typography.displaySmall)
-        Text(stringResource(R.string.history), style = MaterialTheme.typography.titleLarge)
+        Text(
+            stringResource(R.string.track_your_activities),
+            style = MaterialTheme.typography.titleLarge
+        )
         DatePicker(
             state = datePickerState,
             showModeToggle = false,
@@ -64,16 +78,33 @@ fun HistoryScreen(
                 todayDateBorderColor = Primary,
                 currentYearContentColor = Primary,
             ),
-            title = {  },
+            title = { },
             headline = {
                 Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
                 ) {
-                    Text("Tanggal: ${datePickerState.selectedDateMillis?.toDate()}", style = MaterialTheme.typography.bodyLarge)
-                    Text("Perasaan: ", style = MaterialTheme.typography.bodyLarge)
-                    CustomFilledButton(onClick = { toJournal() }, text = "Cek Cerita", modifier = Modifier.align(Alignment.End))
-                    Spacer(modifier = Modifier.height(16.dp))
+                    viewModel.uiState.collectAsState().value.let { state ->
+                        when (state) {
+                            is UiState.Loading -> Text("Loading")
+                            is UiState.Success -> {
+                                Text("Tanggal: ${datePickerState.selectedDateMillis?.toDate()}", style = MaterialTheme.typography.bodyLarge)
+                                if (selectedDateJournal != null) {
+                                    Text(
+                                        "Perasaan: ${selectedDateJournal!!.mood}",
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                    CustomFilledButton(
+                                        onClick = { toJournal(selectedDateJournal!!.journalId) },
+                                        text = "Cek Cerita",
+                                        modifier = Modifier.align(Alignment.End)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(16.dp))
+                            }
+                            is UiState.Error -> Text("Error")
+                        }
+                    }
                 }
             },
         )
--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/history/HistoryViewModel.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/history/HistoryViewModel.kt
@@ -1,10 +1,83 @@
 package com.dailycloud.dailycloud.ui.screen.history
 
+import android.util.Log
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dailycloud.dailycloud.data.DailyCloudRepository
+import com.dailycloud.dailycloud.data.local.model.Journal
+import com.dailycloud.dailycloud.data.remote.response.JournalsResponse
+import com.dailycloud.dailycloud.ui.common.UiState
+import com.dailycloud.dailycloud.util.Util.toDate
+import com.dailycloud.dailycloud.util.Util.toDay
+import com.dailycloud.dailycloud.util.Util.toMonth
+import com.dailycloud.dailycloud.util.Util.toYear
+import com.google.firebase.Timestamp
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(private val repository: DailyCloudRepository) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<UiState<JournalsResponse>> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState<JournalsResponse>>
+        get() = _uiState
+
+    private val _date: MutableState<Date> = mutableStateOf(Date())
+    val date: State<Date> = _date
+
+    private val _selectedDateJournal: MutableState<Journal?> = mutableStateOf(null)
+    val selectedDateJournal: State<Journal?> = _selectedDateJournal
+
+    private val _currentJournals: MutableState<List<Journal?>> = mutableStateOf(listOf())
+    val currentJournals: State<List<Journal?>> = _currentJournals
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    private val _datePickerState: DatePickerState = DatePickerState(
+        initialSelectedDateMillis = System.currentTimeMillis(),
+        initialDisplayedMonthMillis = null,
+        yearRange = (2023..2030),
+        initialDisplayMode = DisplayMode.Picker,
+    )
+    @OptIn(ExperimentalMaterial3Api::class)
+    val datePickerState: DatePickerState = _datePickerState
+
+    init {
+        getJournals(System.currentTimeMillis())
+    }
+
+    private fun getJournals(date: Long) {
+        _uiState.value = UiState.Loading
+        viewModelScope.launch {
+            repository.getJournals(date.toMonth(), date.toYear()).collect {
+                when (it) {
+                    is UiState.Loading -> { _uiState.value = it }
+                    is UiState.Success -> {
+                        _uiState.value = it
+                        _currentJournals.value = it.data.data ?: listOf()
+                    }
+                    is UiState.Error -> { _uiState.value = it }
+                }
+            }
+        }
+    }
+
+    fun onDateChanged(dateMills: Long) {
+        if (_date.value.month != Date(dateMills).month || _date.value.year != Date(dateMills).year) {
+            getJournals(dateMills)
+        }
+        _selectedDateJournal.value = currentJournals.value.find {
+            it?.date!!.toDay() == dateMills.toDay()
+        }
+        _date.value = Date(dateMills)
+    }
 }
