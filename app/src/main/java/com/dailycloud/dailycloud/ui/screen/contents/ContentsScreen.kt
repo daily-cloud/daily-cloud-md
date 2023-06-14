06e1fbf8883880d2ff0fa26b0840b739bd84b9b5
KaisarV <kaisar.valentino123@gmail.com>
2023-06-14 21:26:03 +0700

    [Contents] Change video to image

--- app/src/main/java/com/dailycloud/dailycloud/ui/screen/contents/ContentsScreen.kt
+++ app/src/main/java/com/dailycloud/dailycloud/ui/screen/contents/ContentsScreen.kt
@@ -1,7 +1,5 @@
 package com.dailycloud.dailycloud.ui.screen.contents
 
-import android.net.Uri
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,24 +13,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
+import coil.compose.AsyncImage
 import com.dailycloud.dailycloud.R
 import com.dailycloud.dailycloud.data.local.model.Content
 import com.dailycloud.dailycloud.ui.common.UiState
 import com.dailycloud.dailycloud.ui.components.ContentListItem
 import com.dailycloud.dailycloud.util.Util
-import com.dailycloud.dailycloud.util.Util.dpToPx
 
 @Composable
 fun ContentsScreen(
@@ -84,7 +76,13 @@ fun Content(
             state = rememberLazyListState(),
         ) {
             item {
-                VideoPlayer("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+                AsyncImage(
+                    model = "https://images.unsplash.com/photo-1604480132736-44c188fe4d20?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb",
+                    contentDescription = "Image",
+                    modifier = Modifier
+                        .height(300.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
                 Spacer(modifier = Modifier.height(8.dp))
             }
             items(contentList) { data ->
@@ -99,36 +97,36 @@ fun Content(
     }
 }
 
-@Composable
-@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
-fun VideoPlayer(url: String) {
-    val context = LocalContext.current
-    val exoPlayer = ExoPlayer.Builder(context).build()
-    val mediaItem = MediaItem.fromUri(Uri.parse(url))
-    exoPlayer.setMediaItem(mediaItem)
-
-    val playerView = PlayerView(context)
-    playerView.player = exoPlayer
-    playerView.setShowShuffleButton(false)
-    playerView.setShowNextButton(false)
-    playerView.setShowPreviousButton(false)
-    playerView.setShowFastForwardButton(false)
-    playerView.setShowRewindButton(false)
-    playerView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300.dpToPx())
-
-    DisposableEffect(
-        AndroidView(
-            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
-            factory = { playerView }
-        )
-    ) {
-
-        exoPlayer.prepare()
-        exoPlayer.playWhenReady = false
-
-        onDispose {
-            exoPlayer.release()
-        }
-    }
-}
+//@Composable
+//@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
+//fun VideoPlayer(url: String) {
+//    val context = LocalContext.current
+//    val exoPlayer = ExoPlayer.Builder(context).build()
+//    val mediaItem = MediaItem.fromUri(Uri.parse(url))
+//    exoPlayer.setMediaItem(mediaItem)
+//
+//    val playerView = PlayerView(context)
+//    playerView.player = exoPlayer
+//    playerView.setShowShuffleButton(false)
+//    playerView.setShowNextButton(false)
+//    playerView.setShowPreviousButton(false)
+//    playerView.setShowFastForwardButton(false)
+//    playerView.setShowRewindButton(false)
+//    playerView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300.dpToPx())
+//
+//    DisposableEffect(
+//        AndroidView(
+//            modifier = Modifier.clip(RoundedCornerShape(8.dp)),
+//            factory = { playerView }
+//        )
+//    ) {
+//
+//        exoPlayer.prepare()
+//        exoPlayer.playWhenReady = false
+//
+//        onDispose {
+//            exoPlayer.release()
+//        }
+//    }
+//}
 
